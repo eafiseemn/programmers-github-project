0563f8a97545a9ed35107ba4321de60eb7aa3cc9
eafiseemn <eafiseemn@gmail.com>
2025-07-18 11:45:04 +0900

    [bug] Login Bug Fix

---
+++ src/components/ReviewImages.tsx
@@ -0,0 +1,15 @@
+// 리뷰 이미지 기능 입니다.
+
+function ReviewImages() {
+
+  const newFunction = ():void => {
+    console.log('bug Fixed!')
+  }
+
+  newFunction();
+
+  return (
+    `<div>ReviewImages</div>`
+  )
+}
+export default ReviewImages
